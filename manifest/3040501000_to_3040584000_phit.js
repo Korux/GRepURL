1d9d03a21ee2c321e15c138fa9741f6c9956a1ef
Korux <kevin.dong22@gmail.com>
2025-04-01 17:14:49 -0700

    Delete manifest/3040501000_to_3040584000_phit.js

--- manifest/3040501000_to_3040584000_phit.js
+++
@@ -1,1 +0,0 @@
-define(["jquery","backbone"],function(a,b){var c=b.Model.extend({defaults:{manifest:[{src:Game.imgUri+"/sp/cjs/phit_3040501000_02.png",id:"phit_3040501000_02",type:"image"}]}});return c});
