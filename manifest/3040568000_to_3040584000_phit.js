933306e7340d4ea4d29d9b02b44a6272cefe4365
Korux <kevin.dong22@gmail.com>
2025-04-08 23:07:58 -0700

    Add File: manifest/3040568000_to_3040584000_phit.js

---
+++ manifest/3040568000_to_3040584000_phit.js
@@ -0,0 +1,1 @@
+define(["jquery","backbone"],function(a,b){var c=b.Model.extend({defaults:{manifest:[{src:Game.imgUri+"/sp/cjs/phit_3040568000.png",id:"phit_3040568000",type:"image"}]}});return c});
