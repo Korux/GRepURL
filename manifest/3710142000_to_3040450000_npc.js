ef3d6a6eab2dc930adda025ef45de3756981c232
Korux <kevin.dong22@gmail.com>
2025-04-16 01:40:54 -0700

    Delete File: manifest/3710142000_to_3040450000_npc.js

--- manifest/3710142000_to_3040450000_npc.js
+++
@@ -1,1 +0,0 @@
-define(["jquery","backbone"],function(a,b){var c=b.Model.extend({defaults:{manifest:[{src:Game.imgUri+"/sp/cjs/npc_3710142000_01.png",id:"npc_3710142000_01",type:"image"}]}});return c});
