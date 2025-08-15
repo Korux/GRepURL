7b02848c3b58a832c70bedd231c556e4eb4c417e
Korux <kevin.dong22@gmail.com>
2025-08-15 10:43:08 -0700

    Add File: manifest/3020074000_to_3040610000_npc.js

---
+++ manifest/3020074000_to_3040610000_npc.js
@@ -0,0 +1,1 @@
+define(["jquery","backbone"],function(a,b){var c=b.Model.extend({defaults:{manifest:[{src:Game.imgUri+"/sp/cjs/npc_3020074000_02_a.png",id:"npc_3020074000_02_a",type:"image"},{src:Game.imgUri+"/sp/cjs/npc_3020074000_02_b.png",id:"npc_3020074000_02_b",type:"image"}]}});return c});
